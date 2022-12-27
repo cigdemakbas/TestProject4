810b9c72bffaa1ee953bc09d7cfb462578b1d9ba
Cigdem Akbas <cigdemtazegul@hotmail.com>
2022-12-26 22:54:24 -0500

    forget password improved

--- src/main/java/org/example/registration/ForgetPassword.java
+++ src/main/java/org/example/registration/ForgetPassword.java
@@ -3,5 +3,10 @@ package org.example.registration;
 public class ForgetPassword {
     public static void main(String[] args) {
         System.out.println("hello");
+        System.out.println("this is jack did some addition");
+        for (int i = 0; i <5 ; i++) {
+            System.out.println("i");
+
+        }
     }
 }
