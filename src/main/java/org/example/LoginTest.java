13c0e8d309dd2b2f0232d8516b58ad0c891870fe
Cigdem Akbas <cigdemtazegul@hotmail.com>
2022-12-26 22:26:44 -0500

    login improved

--- src/main/java/org/example/LoginTest.java
+++ src/main/java/org/example/LoginTest.java
@@ -3,5 +3,9 @@ package org.example;
 public class LoginTest {
     public void loginTesting(){
         System.out.println(" i am checking the login");
+        for (int i = 0; i < 5 ; i++) {
+
+        }
     }
+
 }
